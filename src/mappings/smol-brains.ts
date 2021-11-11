080ffac7115990d12d6a60c0900a61edde9f511c
Neil Kistner <neil.kistner@gmail.com>
2021-11-11 17:17:03 -0600

    Update dataSources with correct addresses

--- src/mappings/smol-brains.ts
+++ src/mappings/smol-brains.ts
@@ -1,8 +1,5 @@
 import * as ERC721 from "./721";
-import { log, store } from "@graphprotocol/graph-ts";
-import {
-  Transfer,
-} from "../../generated/TreasureMarketplace/ERC721";
+import { Transfer } from "../../generated/TreasureMarketplace/ERC721";
 import { getOrCreateCollection } from "../helpers";
 
 export function handleTransfer(event: Transfer): void {
@@ -12,10 +9,5 @@ export function handleTransfer(event: Transfer): void {
 
   collection.save();
 
-  // Remove old collection
-  store.remove("Collection", "0xe7ad64be25149d25b3a6ea9556e7d38ec2777b3f");
-  store.remove("Collection", "");
-
   ERC721.handleTransfer(event);
 }
-
