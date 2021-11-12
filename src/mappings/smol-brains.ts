f29ebb1c30cd86b959efebb108a8c02d912e1779
Neil Kistner <neil.kistner@gmail.com>
2021-11-11 21:44:18 -0600

    Remove bad collection and listing

--- src/mappings/smol-brains.ts
+++ src/mappings/smol-brains.ts
@@ -1,4 +1,5 @@
 import * as ERC721 from "./721";
+import { store } from "@graphprotocol/graph-ts";
 import { Transfer } from "../../generated/TreasureMarketplace/ERC721";
 import { getOrCreateCollection } from "../helpers";
 
@@ -8,6 +9,12 @@ export function handleTransfer(event: Transfer): void {
   collection.name = "Smol Brains";
 
   collection.save();
+
+  store.remove("Collection", "");
+  store.remove(
+    "Listing",
+    "0x362b122b187b54161fe958ba67ec6e2927488a27-0x7dd3703d160b061813ccc8a0780257e6a21065d5-0x0"
+  );
 
   ERC721.handleTransfer(event);
 }
